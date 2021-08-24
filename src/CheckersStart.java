9a00726e29ef66f66ff65ac7022654601675ac39
Luruu <xluruu@gmail.com>
2021-08-24 16:50:35 +0200

    rules changed

--- src/CheckersStart.java
+++ src/CheckersStart.java
@@ -36,6 +36,7 @@ public class CheckersStart implements ActionListener{
         frameStart = CGO.addFrame("Checkers Game - Luca Rubino 1934 / Renato Esposito 1881", 180, 260, Color.GREEN, false, new FlowLayout(), ICON_PATH);
     
         l0 = CGO.addLabel("CheckersGame",new Font("Verdana", Font.PLAIN, 18));
+        
         l1 = CGO.addLabel("table size");
         
         String[] someStrings = { "4", "6", "8", "10", "12", "14", "16"};
@@ -102,11 +103,11 @@ public class CheckersStart implements ActionListener{
                 break;
             case "2":
                 System.out.println("label info pressed!");
-                JOptionPane.showMessageDialog(frameStart, "Ogni giocatore dispone di N pedine e K Arcieri (con N e K da definire in base alla dimensione del campo da gioco) di colore diverso rispetto a quelle dell'avversario (bianco o nere).\n Il giocatore bianco fa sempre la prima mossa." +
-                "L'obiettivo del gioco è quello di mangiare tutti i pezzi dell'avversario.\n" +
+                JOptionPane.showMessageDialog(frameStart, "Ogni giocatore dispone di N pedine e 2 Arcieri (N in base alla dimensione del campo da gioco) di colore diverso rispetto a quelle dell'avversario.\n Il giocatore verde fa sempre la prima mossa." +
+                "L'obiettivo del gioco è quello di mangiare tutti i pezzi dell'avversario o di fare il miglior punteggio entro il tempo limite\n" +
                 "Sul campo da gioco sono presenti i seguenti pezzi:\n" + "Pedina: pezzo classico che si muove solamente in diagonale di una casella alla volta e soltanto in avanti. Quando una pedina raggiunge una delle caselle dell'ultima riga viene promossa diventando dama.Dama: upgrade della pedina.\n Può muoversi in più direzioni ad un passo alla volta."+
-                "\nArciere: Se l'arciere mangia un pezzo, resuscita una pedina. Invece se viene mangiato la pedina avversaria diventa dama. Un mago non può diventare dama e non può mangiare un altro mago.... Ulteriori pezzi potrebbero essere aggiunti.\nLa partita termina quando uno dei due giocatori finisce tutti i pezzi, quando un giocatore abbandona o quando il timer scade e in questo caso vince il giocatore che ha il punteggio più alto." +
-                "\nIl punteggio è dato dalla tipologia e dalla quantità di pezzi mangiati, in particolare:\n Pedina: 1 punto.\n Mago: 3 punti.");
+                "\nMago: Se il mago mangia un pezzo, resuscita una pedina. Invece se viene mangiato la pedina avversaria diventa dama. Un mago non può diventare dama e non può mangiare un altro mago.\n" +
+                "\nIl punteggio è dato dalla tipologia e dalla quantità di pezzi mangiati, in particolare:\n Pedina: 1 punto.\n Mago: 3 punti.\n Dama: 5 punti");
             default:
                 System.out.println("case default: " + action + listActionCommands.get(Integer.parseInt((String)action)) + ": pressed!");
         }
@@ -132,8 +133,7 @@ public class CheckersStart implements ActionListener{
     
     private void startGame(String p1Name, String p2Name, int DIM_TABLE, int DIM_BOX) throws Exception{
         Box.DIM_BOX = DIM_BOX;
-        scaleDimensionTable();
-        //N.B: Game Window sizes are (DIM * Box.DIM_BOX, Box.DIM * DIM_BOX)
+        scaleDimensionTable(); //N.B: Game Table sizes are always (DIM * Box.DIM_BOX, Box.DIM * DIM_BOX)
         CheckersTable table = CheckersTable.getInstance(DIM_TABLE, DIM_TABLE);
         Creator factoryM = new ConcreteFactoryM();
         Player pl1 = (Player) factoryM.factoryMethod(p1Name, Color.red, null);
