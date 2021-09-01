189affd35d44f2aa608a756b02ff0a3b84ffb1aa
Luruu <xluruu@gmail.com>
2021-09-01 10:34:59 +0200

    Main is better as name class

--- src/Game/Windows/Start/Main.java
+++ src/Game/Windows/Start/Main.java
@@ -3,7 +3,7 @@ package Game.Windows.Start;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
-public class MyThread implements Runnable {
+public class Main implements Runnable {
     
     public void run(){
         //CheckersStart.getIstance();
@@ -17,7 +17,7 @@ public class MyThread implements Runnable {
 class ThreadPool{
     public static void main(String[] args) {
         ThreadPoolExecutor tpe = (ThreadPoolExecutor) Executors.newFixedThreadPool(6);
-        tpe.execute(new MyThread());
+        tpe.execute(new Main());
         tpe.shutdown();
     }
 }
